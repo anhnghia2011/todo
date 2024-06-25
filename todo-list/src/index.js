f92bba7d0e0be7718da83a4c22f9857a808a370a
anhnghia2011 <nguyenanhnghia81@gamil.com>
2024-06-25 15:34:51 +0700

    lmlai

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
 import App from './App';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom';
+=======
+>>>>>>> parent of 3908e39 (cònlicts)
 
 ReactDOM.render(
   <React.StrictMode>
