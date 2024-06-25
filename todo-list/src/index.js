18fe332d2bad2799bed877167f2b6571b44fdbd7
anhnghia2011 <148032939+anhnghia2011@users.noreply.github.com>
2024-06-25 13:45:40 +0700

    Update index.js

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
 import App from './App';
+<<<<<<< HEAD
 import ReactDOM from 'react-dom';
+=======
+import app
+>>>>>>> b7be749 (Update index.js)
 
 ReactDOM.render(
   <React.StrictMode>
